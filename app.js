2e06e75b7b32305efdefad661f3e1847700edc7f
moonisraza <moonisraza53@gmail.com>
2024-09-11 10:44:18 -0700

    somthing changes

--- app.js
+++ app.js
@@ -5,84 +5,88 @@ const firebaseConfig = {
     storageBucket: "fire-base-sign-in-practice.appspot.com",
     messagingSenderId: "999920116269",
     appId: "1:999920116269:web:529c51bb417eb9db744ea4",
-    measurementId: "G-R94D42KP0Q"
-};
-
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore()
-function signUp() {
-    var fistName = document.getElementById("firstName").value
-    var lastName = document.getElementById("lastName").value
-    var email = document.getElementById("signUpEmail").value
-    var password = document.getElementById("password").value
-
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // Signed in 
-            var user = userCredential.user;
-            console.log(user)
-            db.collection("susesrData").add({
-                fistName: fistName,
-                lastName: lastName,
-                email: email,
-                uid: user.uid,
-
-            }).then((susesrData) => {
-                console.log(susesrData)
-            })
-                .catch((error) => {
-                    console.log("error", error)
-                })
-            // ...
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorMessage)
-            // ..
-        });
-
-}
-
-
-
-function signIn() {
-    var email2 = document.getElementById("signINEmail").value;
-    var password2 = document.getElementById("password2").value;
-
-    firebase.auth().signInWithEmailAndPassword(email2, password2)
-        .then((userCredential) => {
-            var user = userCredential.user;
-
-            // Console mein user data
-            console.log(user);
-
-            // Firestore se user ka data retrieve karna
-            db.collection("susesrData").where("uid", "==", user.uid)
-                .get()
-                .then((querySnapshot) => {
-                    querySnapshot.forEach((doc) => {
-                        var userData = doc.data();
-
-                        // Webpage par user ka data show karna
-                        var userInfoDiv = document.getElementById("userInfo").innerHTML = `
-                            <p>First Name: ${userData.fistName}</p>
-                            <p>Last Name: ${userData.lastName}</p>
-                            <p>Email: ${userData.email}</p>
-                        `;
-                        document.getElementById("userInfo").style.display = "block";
-                        alert(userInfoDiv)
-                    });
-                })
-                .catch((error) => {
-                    console.log("Error getting documents: ", error);
-                });
-
-        })
-        .catch((error) => {
-            console.log(error.message);
-        });
-}
-
-
+    measurementId: "G-R94D42KP0Q",
+  };
+  
+  var provider = new firebase.auth.GoogleAuthProvider();
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  var db = firebase.firestore();
+  
+  function signUp() {
+    var fistName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("signUpEmail").value;
+    var password = document.getElementById("password").value;
+  
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        console.log(user);
+        db.collection("userData")
+          .add({
+            fistName: fistName,
+            lastName: lastName,
+            email: email,
+            uid: user.uid,
+          })
+          .then((susesrData) => {
+            console.log(susesrData);
+          })
+          .catch((error) => {
+            console.log("error", error);
+          });
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        // ..
+      });
+  }
+  
+  function signIn() {
+    var email2 = document.getElementById("signINEmail")
+    var password2 = document.getElementById("password2")
+  
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email2.value, password2.value)
+      .then((userCredential) => {
+        var user = userCredential.user;
+  
+        // Console mein user data
+        console.log(user);
+  
+        // Firestore se user ka data retrieve karna
+        db.collection("userData")
+          .where("uid", "==", user.uid)
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              var userData = doc.data();
+              console.log("userData", userData);
+              // Webpage par user ka data show karna
+              var userInfoDiv = (document.getElementById("userInfo").innerHTML = `
+                              <p>First Name: ${userData.fistName}</p>
+                              <p>Last Name: ${userData.lastName}</p>
+                              <p>Email: ${userData.email}</p>
+                          `);
+              document.getElementById("userInfo").style.display = "block";
+              alert(userInfoDiv);
+            });
+          })
+          .catch((error) => {
+            console.log("Error getting documents: ", error);
+          });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+  
